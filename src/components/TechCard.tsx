9b5592298b48a0d0263dc9464a2c9742342b166d
Valentina1012 <valentiki15@gmail.com>
2026-05-28 16:47:19 -0300

    Se actualizó el componente TechCard para que no use la prop name

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -6,13 +6,11 @@ type TechCardProps = {
 };
 
 export default function TechCard({
-  name = 'Tech',
   icon = <React />,
 }: TechCardProps) {
   return (
     <div className='flex flex-col bg-miscellaneous-primary p-l gap-l items-center justify-center rounded-full border border-neutral-inverse-primary w-50 h-50'>
       <span className='text-background-neutral-disabled'>{icon}</span>
-      {/* <p className='text-neutral-secondary'>{name}</p> */}
     </div>
   );
 }
